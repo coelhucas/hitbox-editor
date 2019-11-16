1e274621c5ee88a96233f4c6e960c8f9ab6693c7
lcrabbit <lucascoelhodacosta@gmail.com>
2019-11-16 11:02:02 -0300

    Add new features

--- Scripts/Canvas.gd
+++ Scripts/Canvas.gd
@@ -1,38 +1,43 @@
 extends CanvasItem
 
-onready var animation_player: AnimationPlayer = $CurrentSprite/Alala_walk/AnimationPlayer
+onready var animation_player: AnimationPlayer = $CurrentSprite/Player/AnimationPlayer
 
-const GRID_LINE_SIZE: int = 500
+const GRID_LINE_SIZE: int = 200
 export var GRID_COLOR: Color
+export var BACKGROUND_COLOR: Color
 
 const ZOOM_AMOUNT: int = 2
-const CAM_SPEED: float = 10.0
+const CAM_SPEED: float = 25.0
 const MAX_ZOOM_OUT: Vector2 = Vector2(1, 1)
 
 var tile_size: int = 1
 var grid_ready: bool = false
 var can_zoom: bool = true
 
+var old_animation_position: float = 0.0
+
 func _process(delta):
 	
 	$Cursor.rect_position = get_global_mouse_position()
 	
 	if Input.is_action_pressed("cam_down"):
-		$Camera2D.position.y += CAM_SPEED
+		$Camera2D.position.y += CAM_SPEED * $Camera2D.zoom.x
 	if Input.is_action_pressed("cam_up"):
-		$Camera2D.position.y -= CAM_SPEED
+		$Camera2D.position.y -= CAM_SPEED * $Camera2D.zoom.x
 	if Input.is_action_pressed("cam_right"):
-		$Camera2D.position.x += CAM_SPEED
+		$Camera2D.position.x += CAM_SPEED * $Camera2D.zoom.x
 	if Input.is_action_pressed("cam_left"):
-		$Camera2D.position.x -= CAM_SPEED
+		$Camera2D.position.x -= CAM_SPEED * $Camera2D.zoom.x
 	
 	if Input.is_action_just_pressed("ui_right"):
-		$CurrentSprite/Alala_walk.frame += 1
+		$CurrentSprite/Player/AnimationPlayer.seek($CurrentSprite/Player/AnimationPlayer.current_animation_position + 0.1, true)
 		
-		if $CurrentSprite/Alala_walk.frame + 1 > $CurrentSprite/Alala_walk.hframes - 1:
-			$CurrentSprite/Alala_walk.frame = 0
 	elif Input.is_action_just_pressed("ui_left"):
-		$CurrentSprite/Alala_walk.frame -= 1
+		$CurrentSprite/Player/AnimationPlayer.seek($CurrentSprite/Player/AnimationPlayer.current_animation_position - 0.1, true)
+	
+	if old_animation_position != $CurrentSprite/Player/AnimationPlayer.current_animation_position:
+		$CanvasLayer/HBoxContainer/CurrentFrameLabel.text = "Frame: " + str(int($CurrentSprite/Player/AnimationPlayer.current_animation_position * 10))
+		old_animation_position = $CurrentSprite/Player/AnimationPlayer.current_animation_position		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
@@ -48,7 +53,7 @@ func zoom_in():
 	if $Camera2D.zoom.x > 0.0625 and can_zoom:
 		can_zoom = false
 		$ZoomDelay.start()
-		$Camera2D.global_position = $MiddlePoint.global_position
+		$Camera2D.global_position = get_global_mouse_position()
 		$Camera2D.zoom = $Camera2D.zoom / ZOOM_AMOUNT
 		print(1 / $Camera2D.zoom.x)
 	
@@ -56,12 +61,14 @@ func zoom_out():
 	if $Camera2D.zoom.x < 0.25 and can_zoom:
 		can_zoom = false
 		$ZoomDelay.start()
-		$Camera2D.global_position = $MiddlePoint.global_position
+		$Camera2D.global_position = get_global_mouse_position()
 		$Camera2D.zoom = $Camera2D.zoom * ZOOM_AMOUNT
 		print(1 / $Camera2D.zoom.x)
 
 func _draw():
 	if not grid_ready:
+		var rect: Rect2 = Rect2(Vector2(0, 0), Vector2(300, 200))
+		draw_rect(rect, BACKGROUND_COLOR)
 		for i in range(GRID_LINE_SIZE):
 			if i % tile_size == 0:
 				draw_line(Vector2(i, 0), Vector2(i, GRID_LINE_SIZE), GRID_COLOR)
@@ -73,3 +80,7 @@ func _draw():
 
 func _on_ZoomDelay_timeout():
 	can_zoom = true
+
+
+func _on_Sccreen_resized():
+	grid_ready = false
--- Scripts/HitBoxManager.gd
+++ Scripts/HitBoxManager.gd
@@ -1,31 +1,115 @@
 extends Node2D
 
-var dragging_left_up: bool = false
-var dragging_left_down: bool = false
-var dragging_right_up: bool = false
-var dragging_right_down: bool = false
+export var hitbox_color: Color
+export var hurtbox_color: Color
+
+var vertical_resizing: bool = false
+var horizontal_resizing: bool = false
+
+var first_drag: bool = false
+var start_position: Vector2
+var prev_mouse_pos: Vector2
+
+var is_hovered: bool = false
+var dragging_box: bool = false
+
+enum HIT_TYPE { Hitbox, Hurtbox }
+var hit_type = HIT_TYPE.Hitbox
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	change_hit_type()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if dragging_left_up:
-		$LeftUp.rect_global_position = get_global_mouse_position()
-		var new_shape: ConvexPolygonShape2D = get_new_collision_shape($LeftUp.rect_position, 0)
-		pass
-
-func get_new_collision_shape(position: Vector2, index: int) -> ConvexPolygonShape2D:
-	var polygon_shape: ConvexPolygonShape2D = $Collider.shape
 	
-	polygon_shape.points[index] = position
+	if is_hovered and Input.is_action_just_pressed("mouse_right"):
+		change_hit_type()
 	
-	return polygon_shape
+	if prev_mouse_pos != get_global_mouse_position():
+		handle_update(delta)
 
-func _on_LeftUp_button_down():
-	dragging_left_up = true
+func handle_update(delta):
+	prev_mouse_pos = get_global_mouse_position()
+	
+	$Guide.rect_global_position.x = int($Left.rect_global_position.x) + 1.5
+	$Guide.rect_global_position.y = int($Up.rect_global_position.y) + 1.5
+	$Guide.rect_size.y = int(abs($Collider.shape.extents.y)) * 2
+	$Guide.rect_size.x = int(abs($Collider.shape.extents.x)) * 2
+	
+	$Up.rect_global_position = Vector2(global_position.x, $Collider.global_position.y - abs($Collider.shape.extents.y))
+	$Down.rect_global_position = Vector2(global_position.x, global_position.y + int(abs($Collider.shape.extents.y)))
+	
+	$Left.rect_global_position = Vector2($Collider.global_position.x - abs($Collider.shape.extents.x), global_position.y)
+	$Right.rect_global_position = Vector2($Collider.global_position.x + abs($Collider.shape.extents.x), global_position.y)
+	
+	
+	if is_hovered and Input.is_action_pressed("mouse_left"):
+		position = Vector2(int(get_global_mouse_position().x), int(get_global_mouse_position().y))
+	
+	if vertical_resizing:
+		update_collision_shape("vertical")
+	
+	if horizontal_resizing:
+		update_collision_shape("horizontal")
+
+func change_hit_type() -> void:
+	if hit_type == HIT_TYPE.Hitbox:
+		hit_type = HIT_TYPE.Hurtbox
+		$Guide.color = hurtbox_color
+	else:
+		hit_type = HIT_TYPE.Hitbox
+		$Guide.color = hitbox_color
+	
+func update_collision_shape(axis: String) -> void:
+	var obj_shape: ConvexPolygonShape2D = $Collider.shape
+	
+	if axis == "vertical":
+		obj_shape.extents.y = int(get_global_mouse_position().y) - $Collider.global_position.y
+	elif axis == "horizontal":
+		obj_shape.extents.x = int(get_global_mouse_position().x) - $Collider.global_position.x
+
+func on_up_button_down():
+	if not first_drag:
+		start_position = $Up.rect_global_position
+		first_drag = true
+	vertical_resizing = true
 
 
 func _on_LeftUp_button_up():
-	dragging_left_up = false
+	vertical_resizing = false
+	first_drag = false
+
+
+func _on_Down_button_down():
+	if not first_drag:
+		start_position = $Down.rect_global_position
+		first_drag = true
+	vertical_resizing = true
+
+
+func _on_Down_button_up():
+	vertical_resizing = false
+
+
+func _on_Guide_mouse_entered():
+	is_hovered = true
+
+func _on_Guide_mouse_exited():
+	is_hovered = false
+
+
+func _on_Left_button_down():
+	horizontal_resizing = true
+
+
+func _on_Left_button_up():
+	horizontal_resizing = false
+
+
+func _on_Right_button_down():
+	horizontal_resizing = true
+
+
+func _on_Right_button_up():
+	horizontal_resizing = false
